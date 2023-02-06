dd81ec69e38adb3f7db75212ee3b3651f9737a3a
shuninamarina <marinashunina03@gmail.com>
2023-02-06 21:03:01 +0200

    Update countriesAPI.js

--- src/js/countriesAPI.js
+++ src/js/countriesAPI.js
@@ -1,6 +1,6 @@
 const BASE_URL = 'https://restcountries.com/v3.1/name';
 
-const filter = 'fields=name,capital,population,languages,flags';
+const filter = 'fields=name,capital,populatio,flags,languages';
 
 export const fetchCountryAPI = cityName => {
   return fetch(`${BASE_URL}${cityName}?${filter}`).then(response => {
